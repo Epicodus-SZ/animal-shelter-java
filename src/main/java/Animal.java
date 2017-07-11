2057fb0f53544b30e65258d6c8bda6b8b07bc00d
Steve Zaske <github@zaske.com>
2017-07-11 15:16:08 -0700

    Able to save Animal to Postgres DB

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,29 +1,25 @@
 import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import java.util.List;
 import org.sql2o.*;
 
-enum Gender {
-  M, F
-}
-
-enum Type {
-  CAT, DOG
-}
+enum Gender { MALE, FEMALE; }
+enum Animal_Type { CAT, DOG; }
 
 public class Animal {
   private int id;
   private String name;
   private Gender gender;
-  private Type animal_type;
-  private LocalDateTime admitted;
+  private Animal_Type animal_type;
+  private LocalDateTime admit;
   private String breed;
 
-  public Animal(String name, Gender gender, Type animal_type, String breed){
+  public Animal(String name, Gender gender, Animal_Type animal_type, String breed){
     this.name = name;
     this.gender = gender;
     this.animal_type = animal_type;
     this.breed = breed;
-    this.admitted = LocalDateTime.now();
+    this.admit = LocalDateTime.now();
   }
 
   public String getName(){
@@ -46,26 +42,29 @@ public class Animal {
       Animal newAnimal = (Animal) otherAnimal;
       return this.getName().equals(newAnimal.getName()) &&
              this.getId() == newAnimal.getId() &&
-             this.getBreed() == newAnimal.getBreed();
+             this.getBreed().equals(newAnimal.getBreed());
     }
   } //end of equals
 
   public static List<Animal> all() {
-     String sql = "SELECT id, name, gender, breed FROM animals";
+     String sql = "SELECT id, name, gender, animal_type, breed, admit FROM animals";
      try(Connection con = DB.sql2o.open()) {
        return con.createQuery(sql).executeAndFetch(Animal.class);
      }
    }
 
   public void save() {
-  try(Connection con = DB.sql2o.open()) {
-    String sql = "INSERT INTO animals (name, gender, breed) VALUES (:name, :gender, :breed);";
-    this.id = (int) con.createQuery(sql, true)
-      .addParameter("name", this.name)
-      .addParameter("gender", this.gender)
-      .addParameter("breed", this.breed)
-      .executeUpdate()
-      .getKey();
-  }
+    Timestamp timestamp = Timestamp.valueOf(this.admit); //need to convert LocalDateTime to Timestamp
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO animals (name, gender, animal_type, breed, admit) VALUES (:name, CAST(:gender AS gender), CAST(:animal_type AS animal_type), :breed, :admit);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("gender", this.gender)
+        .addParameter("animal_type", this.animal_type)
+        .addParameter("breed", this.breed)
+        .addParameter("admit", timestamp)
+        .executeUpdate()
+        .getKey();
+    }
 }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -13,35 +13,35 @@ public class AnimalTest {
   @After
   public void tearDown() {
     try(Connection con = DB.sql2o.open()) {
-      String deleteTasksQuery = "DELETE FROM animals *;";
-      String deleteCategoriesQuery = "DELETE FROM customers *;";
-      con.createQuery(deleteTasksQuery).executeUpdate();
-      con.createQuery(deleteCategoriesQuery).executeUpdate();
+      String deleteAnimalsQuery = "DELETE FROM animals *;";
+      // String deleteCustomersQuery = "DELETE FROM customers *;";
+      con.createQuery(deleteAnimalsQuery).executeUpdate();
+      // con.createQuery(deleteCustomersQuery).executeUpdate();
     }
   }
 
   @Test
   public void Animal_instantiatesCorrectly_true() {
-    Animal testAnimal = new Animal("Ranger", "M", "American Shorthair");
+    Animal testAnimal = new Animal("Ranger", Gender.MALE, Animal_Type.CAT, "American Shorthair");
     assertEquals(true, testAnimal instanceof Animal);
   }
 
   @Test
   public void all_returnsAllInstancesOfAnimal_true() {
-    Animal testAnimal = new Animal("Ranger", "M", "American Shorthair");
+    Animal testAnimal = new Animal("Ranger", Gender.MALE, Animal_Type.CAT, "American Shorthair");
     testAnimal.save();
-    Animal testAnimal2 = new Animal("Dallas", "F", "American Shorthair");
+    Animal testAnimal2 = new Animal("Roverita", Gender.FEMALE, Animal_Type.DOG, "Pitbull");
     testAnimal2.save();
     assertEquals(true, Animal.all().get(0).equals(testAnimal));
     assertEquals(true, Animal.all().get(1).equals(testAnimal2));
   }
 
-  // @Test
-  // public void save_assignsIdToObject() {
-  //   Animal testAnimal = new Animal("Ranger", "M", "American Shorthair");
-  //   testAnimal.save();
-  //   Task savedTask = Task.all().get(0);
-  //   assertEquals(myTask.getId(), savedTask.getId());
-  // }
+  @Test
+  public void save_assignsIdToObject() {
+    Animal testAnimal = new Animal("Ranger", Gender.MALE, Animal_Type.CAT, "American Shorthair");
+    testAnimal.save();
+    Animal dbAnimal = Animal.all().get(0);
+    assertEquals(testAnimal.getId(), dbAnimal.getId());
+  }
 
 } // end of AnimalTest class
